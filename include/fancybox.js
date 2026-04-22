72ed4a9f3931c325c5186f62f86da97bed7ea8a1
Anver Housseini <anver@anykey.ch>
2026-04-22 21:04:32 +0200

    Linting

--- include/fancybox.js
+++ include/fancybox.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-	
+
 	$(".fancybox").fancybox({
 		openEffect : 'fade',
 		closeEffect : 'fade',
 		closeBtn : 'true',
 	});
-			
+
 });
 
 $(document).ready(function() {
